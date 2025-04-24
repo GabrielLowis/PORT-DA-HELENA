e4662dd543f48ad1a202eed53bb99ff6916e9441
GabrielLowis <gabriel.ramos.dev2024@gmail.com>
2025-04-24 15:57:10 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import "./styles/app.css";
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <div>
+          <img src="src/assets/imgs/logo.png" alt="Logo da Helena muito bonito" />
+        </div>
+
+        <div className="links">
+          <p>Habilidades</p>
+          <p>Quem sou?</p>
+          <p>Meus projetos</p>
+          <p>Certificados</p>
+          <p>Contato</p>
+        </div>
+
+      </header>
+
+      {/* <div></div> */}
+    </>
+  )
+}
+
+export default App
