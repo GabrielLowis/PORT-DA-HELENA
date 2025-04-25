b727bbdd441a77d52f8fdf42b7d1102d2096290a
Gabriel Lowis <gabriel.ramos.dev2024@gmail.com>
2025-04-25 15:51:45 -0300

    Finalização da estrutura básica

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <header>
-        <div>
+        <div className="logo">
           <img src="src/assets/imgs/logo.png" alt="Logo da Helena muito bonito" />
         </div>
 
@@ -19,8 +19,58 @@ function App() {
 
       </header>
 
-      {/* <div></div> */}
+      <main></main>
+
+      <section className="projetos">
+        <div className="card">
+          <img src="https://picsum.photos/300/350" alt="" />
+          <h2>Projeto 1</h2>
+          <button>Veja mais</button>
+        </div>
+        <div className="card">
+          <img src="https://picsum.photos/300/350" alt="" />
+          <h2>Projeto 2</h2>
+          <button>Veja mais</button>
+        </div>        
+        <div className="card">
+          <img src="https://picsum.photos/300/350" alt="" />
+          <h2>Projeto 3</h2>
+          <button>Veja mais</button>
+        </div>
+      </section>
+
+      <section className="quemSou">
+
+      </section>
+
+      <section className="algunsDeles">
+        <div className="umDeles">
+          <img src="https://picsum.photos/300/450" alt="" />
+          <p>Projeto Social</p>
+          <p>Pé no chão</p>
+        </div>
+        <div className="umDeles">
+          <img src="https://picsum.photos/300/450" alt="" />
+          <p>Projeto Social</p>
+          <p>Pé no chão</p>
+        </div>
+        <div className="umDeles">
+          <img src="https://picsum.photos/300/450" alt="" />
+          <p>Projeto Social</p>
+          <p>Pé no chão</p>
+        </div>
+        <div className="umDeles">
+          <img src="https://picsum.photos/300/450" alt="" />
+          <p>Projeto Social</p>
+          <p>Pé no chão</p>
+        </div>
+      </section>
+
+      <section className="habilidades">
+        
+      </section>
     </>
+
   )
 }
 
